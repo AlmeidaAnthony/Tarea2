2d76edfa0d51ed493dd373fbd60f93e81000337b
Anthony <anthony.almeida.villarreal@udla.edu.ec>
2023-04-18 18:50:25 -0500

    Resolucion ejercicio 9

---
+++ Ejercicio9.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    int i;
+    printf("Los numeros terminados en 6 entre 25 y 205 son:\n");
+    for (i = 25; i <= 205; i++) 
+    {
+        if (i % 10 == 6) {
+            printf("%d\n", i);
+        }
+    }
+    return 0;
+}
